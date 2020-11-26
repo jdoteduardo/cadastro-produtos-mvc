563891626f01ab534a29859a2c1b37fbabef5cef
joseeduardo0131gmailcom <joseeduardo0131@gmail.com>
2020-11-26 10:45:51 -0300

    Crud com dotnet core 3

---
+++ CourseMVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Coursemvc; Integrated Security=True");
+        }
+    }
+}
